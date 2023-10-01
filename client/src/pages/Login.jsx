1aa47740758368afe5594c0f6882166bba49b760
Abhishek Naik <abhisheknaikworkspace@gmail.com>
2023-10-01 14:28:29 +0530

    small changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
             navigate("/")
         }
         else{
-            message.error("response.data.message")
+            message.error(response.data.message)
         }
     } catch (error) {
         dispatch(HideLoading())
